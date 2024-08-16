b13b56c6b55b28300d875f59e4427f9bfcbe02f3
Mark Hugh Neri <mh.neri@gmail.com>
2024-08-16 16:09:18 +0800

    wormhole connect demo

---
+++ src/app/wormhole/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import WormholeConnect from '@wormhole-foundation/wormhole-connect';
+
+function WormholeConnectPage() {
+    return (<div>
+        <WormholeConnect />
+    </div>)
+}
+
+export default WormholeConnectPage
